59a98be5e2516c665e6ffa6ee43c64a5295852e3
ICHINOSE Shogo <shogo82148@gmail.com>
2022-12-06 23:08:14 +0900

    add options

--- cli.go
+++ cli.go
@@ -2,9 +2,33 @@ package aarm
 
 import (
 	"context"
+	"flag"
+	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 )
+
+type GlobalOptions struct {
+	Region  string
+	Profile string
+}
+
+func (opts *GlobalOptions) Install(set *flag.FlagSet) {
+	set.StringVar(&opts.Region, "region", "", "aws region")
+	set.StringVar(&opts.Profile, "profile", "", "aws profile")
+}
+
+func (opts *GlobalOptions) newConfig(ctx context.Context) (aws.Config, error) {
+	awsOpts := []func(*config.LoadOptions) error{}
+	if opts.Region != "" {
+		awsOpts = append(awsOpts, config.WithRegion(opts.Region))
+	}
+	if opts.Profile != "" {
+		awsOpts = append(awsOpts, config.WithSharedConfigProfile(opts.Profile))
+	}
+	return config.LoadDefaultConfig(ctx, awsOpts...)
+}
 
 func CLI(ctx context.Context, args []string) (int, error) {
 	if len(args) == 0 {
@@ -12,12 +36,19 @@ func CLI(ctx context.Context, args []string) (int, error) {
 	}
 	switch args[0] {
 	case "init":
-		cfg, err := config.LoadDefaultConfig(ctx)
+		set := flag.NewFlagSet("aarm", flag.ExitOnError)
+		var opts InitOption
+		opts.Install(set)
+		set.Parse(args[1:])
+
+		cfg, err := opts.newConfig(ctx)
 		if err != nil {
 			return 1, err
 		}
 		app := NewApp(cfg)
-		app.Init(ctx, nil)
+		app.Init(ctx, &opts)
+	default:
+		return 1, fmt.Errorf("unknown sub-command: %s", args[0])
 	}
 
 	return 0, nil
--- init.go
+++ init.go
@@ -3,17 +3,30 @@ package aarm
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/apprunner"
 )
 
-type InitOption struct{}
+type InitOption struct {
+	GlobalOptions
+	ServiceArn  string
+	ServiceName string
+	ConfigPath  string
+}
 
-func (app *App) Init(ctx context.Context, opt *InitOption) error {
+func (opts *InitOption) Install(set *flag.FlagSet) {
+	opts.GlobalOptions.Install(set)
+	set.StringVar(&opts.ServiceArn, "service-arn", "", "service arn")
+	set.StringVar(&opts.ServiceName, "service-name", "", "service name")
+	set.StringVar(&opts.ConfigPath, "config-path", "service.json", "config path")
+}
+
+func (app *App) Init(ctx context.Context, opts *InitOption) error {
 	out, err := app.appRunner.DescribeService(ctx, &apprunner.DescribeServiceInput{
-		ServiceArn: aws.String("arn:aws:apprunner:ap-northeast-1:445285296882:service/my-app-runner-test/c155dbd6ccd44807abae9fef34064205"),
+		ServiceArn: aws.String(opts.ServiceArn),
 	})
 	if err != nil {
 		return err
@@ -21,8 +34,9 @@ func (app *App) Init(ctx context.Context, opt *InitOption) error {
 
 	service := importService(out.Service)
 	data, err := json.MarshalIndent(service, "", "  ")
+
 	if err != nil {
 		return err
 	}
-	return os.WriteFile("service.json", data, 0o644)
+	return os.WriteFile(opts.ConfigPath, data, 0o644)
 }
